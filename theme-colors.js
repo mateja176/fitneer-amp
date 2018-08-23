a116d405d20816ec45a55f3f8f6fd4ee60f67bdf
mytee306 <mateja176@gmail.com>
2018-08-23 20:19:55 +0200

    Initial Commit

---
+++ theme-colors.js
@@ -0,0 +1,51 @@
+export const themeColors = [
+  // indigo-blue
+  {
+    [ '--dark-primary' ]: '#303F9F',
+    [ '--default-primary' ]: '#3F51B5',
+    [ '--light-primary' ]: '#C5CAE9',
+    [ '--accent-col' ]: '#448AFF'
+  },
+  // greyCyan
+    {
+      [ '--dark-primary' ]: '#455A64',
+      [ '--default-primary' ]: '#607D8B',
+      [ '--light-primary' ]: '#CFD8DC',
+      [ '--accent-col' ]: '#00BCD4'
+    },
+  // pinkTeal
+    {
+      [ '--dark-primary' ]: '#C2185B',
+      [ '--default-primary' ]: '#E91E63',
+      [ '--light-primary' ]: '#F8BBD0',
+      [ '--accent-col' ]: '#009688'
+    },
+  // pinkBlue
+    {
+      [ '--dark-primary' ]: '#C2185B',
+      [ '--default-primary' ]: '#E91E63',
+      [ '--light-primary' ]: '#F8BBD0',
+      [ '--accent-col' ]: '#03A9F4'
+    },
+  // redIndigo
+    {
+      [ '--dark-primary' ]: '#D32F2F',
+      [ '--default-primary' ]: '#F44336',
+      [ '--light-primary' ]: '#FFCDD2',
+      [ '--accent-col' ]: '#536DFE'
+    },
+  // purple amber
+    {
+      [ '--dark-primary' ]: '#7B1FA2',
+      [ '--default-primary' ]: '#9C27B0',
+      [ '--light-primary' ]: '#E1BEE7',
+      [ '--accent-col' ]: '#FFC107'
+    },
+  // brown blue
+    {
+      [ '--dark-primary' ]: '#5D4037',
+      [ '--default-primary' ]: '#795548',
+      [ '--light-primary' ]: '#D7CCC8',
+      [ '--accent-col' ]: '#448AFF'
+    }
+  ]
